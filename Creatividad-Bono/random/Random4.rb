c734268b57a26d8c6d0bb6144e3b2c316d06b87a
onlinecursos <ramirorr01@yahoo.com.mx>
2016-02-14 17:02:17 -0600

    1era vez carpeta para libro creatividad y pendientes CSS3

---
+++ Creatividad-Bono/random/Random4.rb
@@ -0,0 +1,8 @@
+# Programa para generar números aleatorios, 6
+# Se guarda como Random_6.rb
+# http://stackoverflow.com/questions/198460/how-to-get-a-random-number-in-ruby
+# 20/Mzo/2013
+
+puts 4.times.map{ 1 + Random.rand(4)}
+
+
